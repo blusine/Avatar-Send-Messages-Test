05e2fbf8160a6fa21996d4fc2880f1b0de8379f6
blusine <blusine@hotmail.com>
2023-06-03 09:30:48 +0000

    completed the design of classreport

--- main.py
+++ main.py
@@ -31,25 +31,10 @@ def login():
     if (df is not None) & (not df.empty):
       teachername = df["Teacher"].iloc[0]
       form2 = ReportForm(teachername = teachername, date = dt.today())
-      classroom = request.form.get('classroom')
-      df2 = df["Student Name"]
-      print("df2: ", df2)
-      mycols = ['Attendance', 'Homework', 'Academic Performance', 'Comments?', 'Tardy - Send SMS']
-      #df2 = df2.reindex(df2.columns.tolist() + mycols,axis=1)
-      #df2 = df2.reindex(columns = [*df2.columns.tolist(), *mycols], fill_value=1)
-      #df2[['Attendance', 'Homework', 'Tardy - Send SMS']] == 1
-
-      #df2['Attendance'] = '<input type="checkbox" />'
-      
-      #df.insert(loc = 0,column = 'PPA',value = '<input type="checkbox" />')
-      #df2['Academic Performance'] == 2
-      #df2['Comments?'] == 3
-      #headings = df2.columns.to_list()
-      #df2 = df2.to_json(orient='records')
-      #return redirect(url_for('classreport'))
-      #df2 = df2.to_html(index=False)
-      #print("df2: ", df2)
-      return render_template('classreport.html', form = form2, data = df2)
+      #classroom = request.form.get('classroom')
+      df2 = list(df["Student Name"])
+      headings = ['Attendance', 'Homework', 'Academic Performance', 'Comments?', 'Tardy - Send SMS']
+      return render_template('classreport.html', form = form2, headings = headings, data = df2)
       #return f'<h1> data </h1> <br><br> <h2> {df} </h2'
     else:
       header_message = "Wrong Input"
