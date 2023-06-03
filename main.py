aab0b1efedbc776ccce3c014c1f42b6c2b104d1a
blusine <blusine@hotmail.com>
2023-06-03 08:06:08 +0000

    built classreport frame

--- main.py
+++ main.py
@@ -1,47 +1,72 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
-
-import sys, os
-#sys.path.insert(0, os.getcwd()+"/Static")
-x = os.getcwd()
-import utils
+from flask import Flask, render_template, redirect, url_for, request
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, PasswordField, DateField, RadioField
+from wtforms.validators import InputRequired, Email
+from datetime import date as dt
 from utils import read_data
 
-#df = read_data()
-
 app = Flask('__name__')
+csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = 'Mysecret!'
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.trim_blocks = True
 
 class LoginForm(FlaskForm):
-  username = StringField('username', validators=[InputRequired(),] )
-  password = PasswordField('password', validators=[InputRequired(),] )
-  classroom = StringField('classroom', validators=[InputRequired(),] )
+  username = StringField('Zoom Email:', validators=[InputRequired("Input Required!"),  Email()] )
+  password = PasswordField('Zoom Password:', validators=[InputRequired(),] )
+  classroom = StringField('Class Code:', validators=[InputRequired(),] )
 
-
-@app.route('/', methods = ["GET", "POST"])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    #x = os.getcwd()
-    #df = pd.read_csv('/data/ALC_Class_Report_Test.csv')
-    return f'<h1> Username: {form.username.data} Password: {form.password.data} Classroom: {form.classroom.data} Working Directory: {x} </h1>'
-    #return f'<h1> data </h1> <br><br> <h2> {df} </h2'
+class ReportForm(FlaskForm):
+  date = DateField(description="Class Date")
+  classroom = StringField('Class Code:', validators=[InputRequired(),] )
+  teachername = StringField("Teacher's Name", validators=[InputRequired(),] )
+  difficultylevel = RadioField()
   
-  return render_template('login.html', form = form)
-  #else:
-    #<a href="{{ url_for('page_not_found') }}">Go to home</a>
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("404.html"), 404
+@app.route('/', methods = ["POST", "GET"])
+def login():
+  form = LoginForm(username = "avatarmath.space.9@gmail.com", classroom = "v6sl7dl")
+  if form.validate_on_submit():
+    df = read_data(form.username.data, form.password.data, form.classroom.data)
+    if (df is not None) & (not df.empty):
+      teachername = df["Teacher"].iloc[0]
+      form2 = ReportForm(teachername = teachername, date = dt.today())
+      classroom = request.form.get('classroom')
+      df2 = df["Student Name"]
+      print("df2: ", df2)
+      mycols = ['Attendance', 'Homework', 'Academic Performance', 'Comments?', 'Tardy - Send SMS']
+      #df2 = df2.reindex(df2.columns.tolist() + mycols,axis=1)
+      #df2 = df2.reindex(columns = [*df2.columns.tolist(), *mycols], fill_value=1)
+      #df2[['Attendance', 'Homework', 'Tardy - Send SMS']] == 1
+
+      #df2['Attendance'] = '<input type="checkbox" />'
+      
+      #df.insert(loc = 0,column = 'PPA',value = '<input type="checkbox" />')
+      #df2['Academic Performance'] == 2
+      #df2['Comments?'] == 3
+      #headings = df2.columns.to_list()
+      #df2 = df2.to_json(orient='records')
+      #return redirect(url_for('classreport'))
+      #df2 = df2.to_html(index=False)
+      #print("df2: ", df2)
+      return render_template('classreport.html', form = form2, data = df2)
+      #return f'<h1> data </h1> <br><br> <h2> {df} </h2'
+    else:
+      header_message = "Wrong Input"
+      body_message = "Incorrect Email, Password or Class Code"
+      #return f'<h1> data </h1> <br><br> <h2> {df} </h2'
+      return redirect(url_for('error_message', header_message=header_message , body_message=body_message))
+  return render_template('login.html', form = form)
+
+@app.route('/classreport', methods = ["GET", "POST"])
+def classreport():
+  form2 = ReportForm()
+  return render_template('classreport.html', form = form2)
+
+@app.route('/error/<header_message>/<body_message>', methods = ["GET", "POST"])
+def error_message(header_message, body_message):
+    return render_template("error.html", header_message=header_message, body_message=body_message)
   
 if (__name__) == "__main__":
   import pandas as pd
-  #df = pd.read_csv('/data/ALC_Class_Report_Test.csv')
-  #df = read_data()
-  #print(df)
   app.run(host='0.0.0.0', port=8080, debug = True)
--- utils.py
+++ utils.py
@@ -1,8 +1,43 @@
-def read_data():
+def read_data(username, password, classcode):
   import pandas as pd
-  df = pd.read_csv('/data/ALC_Class_Report_Test.csv')
+  import os
+  x = os.getcwd()
+  if username_validator(username, password):
+    df = pd.read_csv(x + '/data/ALC_Class_Report_Test.csv')
+    df = df[df['Zoom'] == username]
+    df = df[df['ClassCode'] == classcode]
+    cols = ['Parent Name',	'Parent email',	'Student Name',	'AvatarGrade',	'Teacher',	'Zoom',	'Password',	'Parent phone',	'ClassCode']
+    df = df[cols]
+  else:
+    df = pd.DataFrame()
   return df
 
+def username_validator(username, password):
+  import pandas as pd
+  import os
+  x = os.getcwd()
+  df = pd.read_csv(x + '/data/username_validator.csv')
+  cols = ['username',	'password']
+  df[cols] = df[cols].astype(str)
+  df = df[df['username'] == username]
+  if (not df.empty):
+    if (df['password'].iloc[0] == password):
+      return True
+    else:
+      return False
+    return False
+
+def classcode_validator(username, classcode):
+  import pandas as pd
+  import os
+  x = os.getcwd()
+  df = pd.read_csv(x + '/data/classcode_validator.csv')
+  df = df[df['username'] == username]
+  if df & (df['classcode'][0] == classcode):
+    return True
+  else:
+    return False
+    
 def clean_phone(phone):
   return list(map(lambda x: fix_a_num(x), phone))
       
